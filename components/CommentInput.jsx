ec76c0908550f8f83a8aaeca084d1b9fd7b4b9e4
Sammy212 <samuelafo.212@gmail.com>
2024-10-11 15:24:13 +0100

    Post Feed Component | Updated Comment Section UI Component

--- components/CommentInput.jsx
+++ components/CommentInput.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, Button, Flex, Input } from 'antd';
+import { useUser } from '@clerk/nextjs';
+import { Icon } from '@iconify/react';
 
-const CommentInput = () => {
+const CommentInput = ({postId, setExpanded, queryId}) => {
+
+    const {user} = useUser();
+    const [value, setValue] = React.useState("");
   return (
-    <div>CommentInput</div>
+    <Flex gap={"1rem"} align='center'>
+        {/* User avatar */}
+        <Avatar 
+            src={user?.imageUrl} 
+            size={40} style={{ minWidth: "40px"}} 
+        />
+
+        {/* input form */}
+        <Input.TextArea
+            placeholder='Write what you think...'
+            style={{resize: "none"}}
+            autoSize={{ minRows: 1, maxRows: 5 }}
+            value={value}
+            onChange={
+                (e) => calculateSizeAdjustValues(e.target.value)
+            }
+            // disabled
+        />
+
+        <Button
+            type='primary'
+        >
+            <Icon icon={"iconamoon:send-fill"} width={"1.2rem"} />
+        </Button>
+    </Flex>
   )
 }
 
